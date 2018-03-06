dc82b5c0af0a31ef51ccc0766e9a175cc1ccf4fc
Francesco Melchiori <franzmelchiori@gmail.com>
2018-03-06 14:44:38 +0100

    Initial commit

--- influxdb_explorer.py
+++ influxdb_explorer.py
@@ -22,15 +22,73 @@
 import json
 
 
-class DataCustomer:
-    def __init__(self, customer_name):
+class CustomerData:
+    def __init__(self, customer_name, json_path=''):
         self.customer_name = customer_name
-        load_json(self.customer_name)
+        self.json_path = json_path
+        self.check_map = {}
+        self.load_check_map_json()
+
+    def __repr__(self):
+        return '{0}'.format(self.check_map)
+
+    def load_check_map_json(self):
+        if self.json_path:
+            file_path = self.json_path
+        else:
+            file_path = '{0}_check_map.json'.format(self.customer_name)
+        check_map = load_json(file_path)
+        for customer_data in check_map['customers']:
+            if customer_data['customer_name'] == self.customer_name:
+                self.check_map = customer_data
+        if not self.check_map:
+            raise DataNotFound(data_name=self.customer_name,
+                               source_name='customers')
+        return True
 
 
-def load_json(file_name):
+class CustomerInfluxDBData(CustomerData):
+    def __init__(self, customer_name, json_path=''):
+        self.customer_name = customer_name
+        self.json_path = json_path
+        self.data_source_name = ''
+        self.data_source_ip_port = ''
+        self.databases = []
+        self.load_influxdb_check_map()
+
+    def __repr__(self):
+        return '{0}'.format(self.check_map)
+
+    def load_influxdb_check_map(self):
+        customerdata = CustomerData(self.customer_name, self.json_path)
+        for data_source in customerdata.check_map['data_sources']:
+            if data_source['data_source_name'] == 'influxdb':
+                self.data_source_name = data_source['data_source_name']
+                self.data_source_ip_port = data_source['data_source_ip_port']
+                self.databases = data_source['databases']
+        if not self.data_source_name:
+            raise DataNotFound(data_name='influxdb',
+                               source_name='data_sources')
+        return True
+
+
+class DataNotFound(Exception):
+    def __init__(self, data_name='', source_name=''):
+        self.data_name = data_name
+        self.source_name = source_name
+
+    def __str__(self):
+        exception_message = ''
+        if self.data_name:
+            exception_message += "'{0}'".format(self.data_name)
+        if self.source_name:
+            exception_message += " from '{0}'".format(self.source_name)
+        return exception_message
+
+
+def load_json(file_path):
     try:
-        json_file = open('{0}'.format(file_name))
+        json_file = open(file_path)
     except IOError:
         print('error | json file opening issue')
         return False
@@ -43,5 +101,4 @@ def load_json(file_name):
 
 
 if __name__ == '__main__':
-    load_json('bossard_influxdb_testcase_map.json')
-    bla
+    dc = CustomerInfluxDBData('bossard')

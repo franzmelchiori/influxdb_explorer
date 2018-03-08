4ef8994131fa3605cb5a6aa23500088fef520887
Francesco Melchiori <franzmelchiori@gmail.com>
2018-03-08 18:28:48 +0100

    add check_feature_availability method

--- influxdb_explorer.py
+++ influxdb_explorer.py
@@ -82,6 +82,25 @@ class CustomerInfluxDBData(CustomerData):
         return True
 
 
+class CustomerInfluxDBCheck(CustomerInfluxDBData):
+    def __init__(self, customer_name, json_path=''):
+        CustomerInfluxDBData.__init__(self, customer_name, json_path)
+        check_feature_availability(
+            ip=self.data_source_ip_port.split(':')[0],
+            port=self.data_source_ip_port.split(':')[1],
+            database=self.databases[0]['database'],
+            measure=self.databases[0]['measurements'][0]['measurement'],
+            host=self.databases[0]['measurements'][0]['hosts'][0]['host'],
+            testcase=self.databases[0]['measurements'][0]['hosts'][0]['tests'][0]['test_name'],
+            transaction=self.databases[0]['measurements'][0]['hosts'][0]['tests'][0]['transactions'][0]['transaction_name'],
+            feature_name=self.databases[0]['measurements'][0]['hosts'][0]['tests'][0]['transactions'][0]['checks'][0]['feature_name'],
+            measure_unit=self.databases[0]['measurements'][0]['hosts'][0]['tests'][0]['transactions'][0]['checks'][0]['measure_unit'],
+            sanity_period=self.databases[0]['measurements'][0]['hosts'][0]['tests'][0]['transactions'][0]['checks'][0]['sanity_period'])
+
+    def __repr__(self):
+        return 'bla'
+
+
 class DataNotFound(Exception):
     def __init__(self, data_name='', source_name=''):
         self.data_name = data_name
@@ -110,39 +129,130 @@ def load_json(file_path):
     return json_object
 
 
-def get_influxdb_data(ip, database, measure, minutes, port='8086',
-                      features=None, feature_filter=None, feature_order='asc'):
+def get_influxdb_data(ip, database, measure, seconds_from_now, port='8086',
+                      features=None, feature_filter=None, feature_order='desc'):
+    """
+        database: '<influxdb_database_name>'
+        measure: '<influxdb_measurement_name>'
+        features: [<list_of_features_to_fetch>]
+        feature_filter: {<dict_of_features_and_their_values_to_filter_in>}
+        feature_order: 'asc' or 'desc'
+    """
     influxdb_base_url = 'http://{0}:{1}/query'.format(ip, port)
     if features is None:
         features = ['*']
     influxdb_query_features = 'SELECT ' + ', '.join(features)
     influxdb_query_measure = 'FROM {0}'.format(measure)
-    influxdb_query_selection = 'WHERE time > now() - {0}m ' \
-                               'AND time < now()'.format(minutes)
+    influxdb_query_selection = 'WHERE time > now() - {0}s ' \
+                               'AND time < now()'.format(seconds_from_now)
     if feature_filter:
         feature_filter_influxdb_format = ["{0} = '{1}'".format(
             feature_name, feature_value)
             for feature_name, feature_value
             in feature_filter.items()]
-        influxdb_query_selection += 'AND ' + ' AND '.join(
+        influxdb_query_selection += ' AND ' + ' AND '.join(
             feature_filter_influxdb_format)
-    influxdb_query_order = 'ORDER BY time ' + \
-                           'ASC' if feature_order == 'asc' else 'DESC'
+    influxdb_query_order = 'ORDER BY time '
+    if feature_order == 'asc':
+        influxdb_query_order += 'ASC'
+    elif feature_order == 'desc':
+        influxdb_query_order += 'DESC'
     influxdb_query = ' '.join([influxdb_query_features, influxdb_query_measure,
                                influxdb_query_selection, influxdb_query_order])
     influxdb_query_url = urllib.urlencode({'q': influxdb_query,
-                                           'db': database,
-                                           'pretty': 'true'})
+                                           'db': database})
     influxdb_request = '{0}?{1}'.format(influxdb_base_url, influxdb_query_url)
-    influxdb_response = urllib2.urlopen(influxdb_request)
-    # print(influxdb_response.geturl())
-    # print(influxdb_response.info())
-    # print(influxdb_response.getcode())
-    # print(influxdb_response.read())
+    influxdb_response = json.load(urllib2.urlopen(influxdb_request))
+    # print(influxdb_response)
+    # print_influxdb_data(influxdb_response)
     return influxdb_response
 
 
+def print_influxdb_data(influxdb_data):
+    if 'series' in influxdb_data['results'][0].keys():
+        print(influxdb_data['results'][0]['series'][0]['columns'])
+        values = influxdb_data['results'][0]['series'][0]['values']
+        for value in values:
+            print(value)
+        return True
+    else:
+        print('no results')
+        return False
+
+
+def check_feature_availability(ip, port, database, measure, host, testcase,
+                               transaction, feature_name, measure_unit,
+                               sanity_period):
+    feature_filter = {'host': host, 'test_name': testcase,
+                      'transaction_name': transaction}
+    seconds_converter = {'seconds': 1, 'minutes': 60, 'hours': 60*60,
+                         'days': 60*60*24}
+    seconds_from_now = sanity_period * seconds_converter[measure_unit]
+    features = ['time', 'host', 'test_name', 'transaction_name', 'performance',
+                'warning_threshold', 'critical_threshold', 'state']
+    influxdb_response = get_influxdb_data(ip=ip, port=port,
+                                          database=database,
+                                          measure=measure,
+                                          seconds_from_now=seconds_from_now,
+                                          features=features,
+                                          feature_filter=feature_filter,
+                                          feature_order='desc')
+    # print(influxdb_response)
+    # print_influxdb_data(influxdb_response)
+    if 'series' in influxdb_response['results'][0].keys():
+        influxdb_response_features = influxdb_response[
+            'results'][0]['series'][0]['columns']
+        # print(influxdb_response_features)
+    else:
+        print('no results')
+        return False
+    if feature_name in influxdb_response_features:
+        timestamp_index = influxdb_response_features.index('time')
+        feature_index = influxdb_response_features.index(feature_name)
+        measure_points = influxdb_response[
+            'results'][0]['series'][0]['values']
+        check_sequence = []
+        for measure_point in measure_points:
+            # print(measure_point)
+            timestamp = measure_point[timestamp_index]
+            measure_state = measure_point[feature_index]
+            measure_check = 1 if measure_state == 'ok' else 0
+            check_sequence.append((measure_check, timestamp))
+        # print(availability_sequence)
+        check = check_availability_sequence(check_sequence,
+                                            'at_least_one_ok')
+        print(check)
+        return check
+    else:
+        print('no feature')
+        return False
+
+
+def check_availability_sequence(availability_sequence, availability_mode):
+    if availability_mode == 'at_least_one_ok':
+        # print(availability_sequence)
+        for (measure_check, timestamp) in availability_sequence:
+            if measure_check == 1:
+                return 1
+        return 0
+
+
 if __name__ == '__main__':
-    # print(CustomerData('<customer>'))
-    # print(CustomerInfluxDBData('<customer>'))
-    get_influxdb_data('<ip>', '<customer>', 'alyvix', 1)
+    # print(CustomerData('customer'))
+    # print(CustomerInfluxDBData('customer'))
+    CustomerInfluxDBCheck('customer')
+
+    # get_influxdb_data(ip='<ip>',
+    #                   database='<customer_name>',
+    #                   measure='<measurement>',
+    #                   seconds_from_now=<sanity_seconds>)
+    # check_feature_availability(ip='<ip>',
+    #                            port='<port>',
+    #                            database='<customer_name>',
+    #                            measure='<measurement>',
+    #                            host='<host>',
+    #                            testcase='<test_name>',
+    #                            transaction='<transaction_name>',
+    #                            feature_name='<feature_name>',
+    #                            measure_unit='<measure_unit>',
+    #                            sanity_period=<sanity_period>)
